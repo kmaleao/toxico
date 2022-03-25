aa9c98d8cec97981c23420b368bf191bea2184a0
kmaleao <udijonson@gmail.com>
2022-03-25 13:05:39 -0300

    freme

--- pages/index.js
+++ pages/index.js
@@ -28,7 +28,7 @@ export default function Home() {
           {' '}
           
         </p>
-        <iframe width="760" height="515" src="https://www.youtube.com/embed/CuyTC8FLICY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        
 
        
 
